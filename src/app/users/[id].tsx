27e95c07fbe6bed9107f99c7e596265e0b074121
David Lozada <davsh47@hotmail.com>
2023-09-13 18:52:03 -0500

    profile page

---
+++ src/app/users/[id].tsx
@@ -0,0 +1,101 @@
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import userJson from "../../../assets/data/user.json";
+
+export default function Profile() {
+  const [user, setUser] = useState(userJson);
+
+  const { id } = useLocalSearchParams();
+  const navigation = useNavigation();
+
+  const onConnect = () => {
+    console.log("connect");
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: user.name });
+  }, [user?.name]);
+
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        {/* BG image */}
+        <Image source={{ uri: user.backImage }} style={styles.backImage} />
+        <View style={styles.headerContent}>
+          {/* Profile Image */}
+          <Image source={{ uri: user.image }} style={styles.image} />
+          {/* Name and Position */}
+          <Text style={styles.name}>{user.name}</Text>
+          <Text>{user.position}</Text>
+          {/* Connect Button */}
+          <Pressable onPress={onConnect} style={styles.button}>
+            <Text style={styles.buttonText}>COnnet</Text>
+          </Pressable>
+        </View>
+      </View>
+      {/* About */}
+      <View style={styles.about}>
+        <Text style={styles.aboutTitle}>About</Text>
+        <Text style={styles.paragraph}>{user.about}</Text>
+      </View>
+      {/* Experience */}
+      <View style={styles.xp}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  header: {
+    backgroundColor: "white",
+  },
+  xp: {},
+  backImage: {
+    width: "100%",
+    aspectRatio: 5 / 2,
+    marginBottom: -60,
+  },
+  image: {
+    width: 100,
+    aspectRatio: 1,
+    borderRadius: 60,
+    borderWidth: 3,
+    borderColor: "white",
+  },
+  headerContent: {
+    padding: 10,
+    paddingTop: 0,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  /* Button */
+  button: {
+    backgroundColor: "royalblue",
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 50,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+  },
+  /* About */
+  about: {
+    backgroundColor: "white",
+    padding: 10,
+    marginVertical: 10,
+  },
+  aboutTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 5,
+  },
+  paragraph: {
+    lineHeight: 20,
+  },
+});
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -27,13 +27,18 @@ export default function PostListItem({ post }: PostListItemProps) {
     <Link href={`/posts/${post.id}`} asChild>
       <Pressable>
         {/* Header */}
-        <View style={styles.header}>
-          <Image source={{ uri: post.author.image }} style={styles.userImage} />
-          <View>
-            <Text style={styles.username}>{post.author.name}</Text>
-            <Text>{post.author.position}</Text>
-          </View>
-        </View>
+        <Link href={`/users/${post.author.id}`} asChild>
+          <Pressable style={styles.header}>
+            <Image
+              source={{ uri: post.author.image }}
+              style={styles.userImage}
+            />
+            <View>
+              <Text style={styles.username}>{post.author.name}</Text>
+              <Text>{post.author.position}</Text>
+            </View>
+          </Pressable>
+        </Link>
         {/* Text content */}
         <Text style={styles.content}>{post.content}</Text>
 
